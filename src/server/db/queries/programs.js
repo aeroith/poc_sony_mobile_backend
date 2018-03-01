a347e07f4fb2c030d055ea242a97f17253f69410
Serhat Cillidag <serhat.cillidag@sony.com>
2018-03-01 11:12:05 +0300

    Fix indent problem

--- src/server/db/queries/programs.js
+++ src/server/db/queries/programs.js
@@ -2,26 +2,30 @@ const knex = require('../connection');
 
 function getAllPrograms() {
   return knex
-    .select('p.name',
-    'p.id',
-    'p.image_url AS local_image_url',
-    'gp.type',
-    'gp.tags',
-    'gp.featured',
-    'gp.image_url AS global_image_url')
+    .select(
+      'p.name',
+      'p.id',
+      'p.image_url AS local_image_url',
+      'gp.type',
+      'gp.tags',
+      'gp.featured',
+      'gp.image_url AS global_image_url',
+    )
     .from('programs AS p')
-    .innerJoin('global_programs AS gp', 'p.global_program_id', 'gp.id')
+    .innerJoin('global_programs AS gp', 'p.global_program_id', 'gp.id');
 }
 
 function getSingleProgram(id) {
   return knex
-    .select('p.name',
-    'p.id',
-    'p.image_url AS local_image_url',
-    'gp.type',
-    'gp.tags',
-    'gp.featured',
-    'gp.image_url AS global_image_url')
+    .select(
+      'p.name',
+      'p.id',
+      'p.image_url AS local_image_url',
+      'gp.type',
+      'gp.tags',
+      'gp.featured',
+      'gp.image_url AS global_image_url',
+    )
     .from('programs AS p')
     .innerJoin('global_programs AS gp', 'p.global_program_id', 'gp.id')
     .where('p.id', id);
@@ -29,5 +33,5 @@ function getSingleProgram(id) {
 
 module.exports = {
   getAllPrograms,
-  getSingleProgram
+  getSingleProgram,
 };
